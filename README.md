95847af75720968dbf8a08fb2a62394ecee26089
lokeshgupta1981 <howtodoinjava@gmail.com>
2021-12-17 01:52:58 +0530

    Jackson updates

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 -  [GSON Tutorial](https://howtodoinjava.com/gson/google-gson-tutorial/)
 -  [Lo4j2 with SLF4J](https://howtodoinjava.com/log4j2/log4j2-with-slf4j/)
 -  [Lombok](https://howtodoinjava.com/lombok/)
+-  [Jackson JSON](https://howtodoinjava.com/jackson/jackson-read-write-json/)
--- src/main/java/com/howtodoinjava/demo/jackson/Jackson2Demo.java
+++ src/main/java/com/howtodoinjava/demo/jackson/Jackson2Demo.java
@@ -1,20 +1,22 @@
 package com.howtodoinjava.demo.jackson;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Jackson2Demo {
-	public static void main(String[] args) throws JsonProcessingException {
+	public static void main(String[] args) throws IOException {
 		ObjectMapper mapper = new ObjectMapper();
 
 		Article article = new Article(1L, "Test Title", Collections.singletonList("Test Tag"));
 
 		String json = mapper.writeValueAsString(article);
-		
+
 		System.out.println(json);
 
 		json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(article);
@@ -24,6 +26,12 @@ public class Jackson2Demo {
 		json = "{\"id\":1,\"title\":\"Test Title\",\"tags\":[\"Test Tag\"]}";
 
 		Article newArticle = mapper.readValue(json, Article.class);
+
+		System.out.println(newArticle);
+
+		URL url = Jackson2Demo.class.getClassLoader().getResource("article.json");
+
+		newArticle = mapper.readValue(new File(url.getFile()), Article.class);
 
 		System.out.println(newArticle);
 	}
