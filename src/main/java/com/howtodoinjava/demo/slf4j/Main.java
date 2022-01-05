5138f577f7b1f3aa8e66dcc6aab33980a10d6aa2
lokeshgupta1981 <howtodoinjava@gmail.com>
2022-01-05 21:43:56 +0530

    Added Logback color coding

--- src/main/java/com/howtodoinjava/demo/slf4j/Main.java
+++ src/main/java/com/howtodoinjava/demo/slf4j/Main.java
@@ -11,11 +11,9 @@ public class Main {
 
 		logger.debug("Debug Message Logged !!!");
 		logger.info("Info Message Logged !!!");
-		logger.error("Error Message Logged !!!", new NullPointerException("NullError"));
+		logger.error("Error Message Logged !!!", new NullPointerException("Something is NULL"));
 
 		Article a = Article.builder(1L).title("Test Article").tag("Data").build();
 		logger.info("Article fecthed for id : {} is : {}", 1, a);
-
-		logger.info("Article fecthed for id : " + 1 + " is : " + a.toString());
 	}
 }
