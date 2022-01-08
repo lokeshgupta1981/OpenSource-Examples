abb8ff3f03cde90c9769fb7b1ca5008423b1dcbd
lokeshgupta1981 <howtodoinjava@gmail.com>
2022-01-08 21:14:11 +0530

    data masking example

--- src/main/java/com/howtodoinjava/demo/slf4j/Main.java
+++ src/main/java/com/howtodoinjava/demo/slf4j/Main.java
@@ -1,5 +1,9 @@
 package com.howtodoinjava.demo.slf4j;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -8,15 +12,29 @@ import com.howtodoinjava.demo.lombok.Article;
 
 public class Main {
 	public static void main(final String[] args) {
+		
+		System.setProperty("ENV", "prod");
+		
 		Logger logger = LoggerFactory.getLogger(Main.class);
 		
 		MDC.put("MDC_KEY", "VALUE");
 
-		logger.debug("Debug Message Logged !!!");
-		logger.info("Info Message Logged !!!");
-		logger.error("Error Message Logged !!!", new NullPointerException("Something is NULL"));
-
-		Article a = Article.builder(1L).title("Test Article").tag("Data").build();
-		logger.info("Article fecthed for id : {} is : {}", 1, a);
+		/*
+		 * logger.debug("Debug Message Logged !!!");
+		 * logger.info("Info Message Logged !!!");
+		 * logger.error("Error Message Logged !!!", new
+		 * NullPointerException("Something is NULL"));
+		 * 
+		 * Article a = Article.builder(1L).title("Test Article").tag("Data").build();
+		 * logger.info("Article fecthed for id : {} is : {}", 1, a);
+		 */
+		
+		Map<String, String> customer = new HashMap<String, String>();
+		
+		customer.put("id", "12345");
+		customer.put("ssn", "856-45-6789");
+		customer.put("email", "admin@email.com");
+		
+		logger.info("Customer found : {}", new JSONObject(customer));
 	}
 }
